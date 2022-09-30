7f28a7fce29712bea0d09e9762add9b91d7c2b30
Martin <mvanlaethem0@gmail.com>
2022-09-29 17:57:03 -0700

    Moving threadJob class out of thread class file

--- Engine/src/Engine/Thread.h
+++ Engine/src/Engine/Thread.h
@@ -1,42 +1,28 @@
 #pragma once
 #include <thread>
+#include "ThreadJob.h"
 
 // this is the standard for each thread
 
 namespace engine_concurrent
 {
-	// Things for the threads to do
-	class ThreadJob
-	{
-		// threadJobs need to take in a generic set of arguments
-		// and return something generic to the caller
-	public:
-		void run();
+    // Wrapper class for the actual threads
+    class Thread
+    {
+    public:
+        //TODO: Threads need a null job to start with
+        explicit Thread(); // explicit constructor for threads
+        void assignJob(ThreadJob*);
+        ThreadJob* getJob();
+        void run();
+        void init();
 
-	protected:
-		//
+    protected:
+        //
 
-	private:
-		int count; // 
-	};
+    private:
+        std::thread* m_thread;
+        ThreadJob* m_threadJob; // pointer to the thread's current job
 
-	// Wrapper class for the actual threads
-	class Thread
-	{
-	public:
-		//TODO: Threads need a null job to start with
-		explicit Thread(); // explicit constructor for threads
-		void assignJob(ThreadJob*);
-		ThreadJob* getJob();
-		void run();
-		void init();
-
-	protected:
-		//
-
-	private:
-		std::thread* m_thread;
-		ThreadJob* m_threadJob; // pointer to the thread's current job
-
-	};
+    };
 }
