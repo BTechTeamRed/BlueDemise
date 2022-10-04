b553de45afd1018dee916610332bca8bfa4ce40a
Snow <snow.nh106@gmail.com>
2022-10-03 23:29:44 -0700

    Delta Time implementation
    Added to relevant methods in Scene.cpp

---
+++ Engine/src/Engine/DeltaTime.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace Engine
+{
+	//Container for the DeltaTime used in game.
+	class DeltaTime
+	{
+	public:
+		DeltaTime(double dt = 0)
+		: m_Time(dt)
+		{
+		}
+
+		operator double() const { return m_Time; }
+
+		double getMilliseconds() const { return m_Time * 1000; }
+		double getSeconds() const { return m_Time; }
+
+		//Updates the value of deltaTime.
+		void updateDeltaTime()
+		{
+			const double time = glfwGetTime();
+			m_Time = time - m_previousTime;
+			m_previousTime = time;
+		}
+
+	private:
+		double m_Time;
+		double m_previousTime;
+	};
+}
