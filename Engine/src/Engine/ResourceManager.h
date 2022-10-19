f8a3d6db72d8d0638accdba1afa9acba56ca0c2b
KevinAlexV <32602411+KevinAlexV@users.noreply.github.com>
2022-10-19 13:38:20 -0700

    Sprite rendering (#21)
    * Added ToDos for sprite rendering
    
    * Changed triangle to quad
    
    * UV mapping of quad
    
    * Setup function to load images
    
    * Update ResourceManager.cpp
    
    * initial setup of textures (not working right)
    
    * Update Scene.cpp
    
    * png test
    
    * Sectioned off texture binding (MEMORY LEAKS PRONE)
    
    This is not effectively managing memory, and will need to be fixed. This is just the first working version where textures are bound in a resource manager, and will need to be altered to effectively manage textures and images.
    
    * Memory mgmt adjustments
    
    * Texture storage enabled
    
    * Update ResourceManager.cpp
    
    * Comment update
    
    * Beginning component integration
    
    * Multiple sprites now rendering
    
    * Icon for engine window
    
    * Functioning and commenting
    
    * Update Scene.h
    
    * Adjustments based on PR comments
    
    * Adjustments
    
    * Update Components.h
    
    Co-authored-by: Hevano <evanohirabe@gmail.com>
    Co-authored-by: Snow <snow.nh106@gmail.com>

--- Engine/src/Engine/ResourceManager.h
+++ Engine/src/Engine/ResourceManager.h
@@ -1,107 +1,133 @@
 #pragma once
 
 #include "Core.h"
-#include "json.h"
 #include "Log.h"
-#include <iostream>
-#include <fstream>
-#include<sstream>
+#include "json.h"
 #include <string>
 #include <filesystem>
-#include <map>
 
 //Written by Kevin Vilanova, KevinAlexV.
 //Class is intended to import and manage assets and resources used by the game engine.
+//Documentation for the stb_image library can be found here: https://github.com/nothings/stb/blob/master/stb_image.h | https://gist.github.com/roxlu/3077861
+//For texture loading, the expectation is to bind a texture then render any objects using that texture before switching to another texture, as opposed to doing a load 
+//per use-case (ie; do bind text 1 -> draw object A B C, then bind text 2).
+struct GLFWwindow;
+
 namespace Engine
 {
+	typedef unsigned int GLuint;
 
 	class ENGINE_API ResourceManager
 	{
+
+	public:
+
+		//A struct to contain all data contained within an STB image. 
+		struct ImageData
+		{
+			unsigned char* image;
+			int width, height, numComponents;
+		};
+
+		#pragma region Singleton Instance Management
+
+		//Singletons should not be cloneable, this is to prevent clones.
+		ResourceManager(ResourceManager& other) = delete;
+
+		//Singletons should not be assignable, this is to prevent that.
+		void operator=(const ResourceManager&) = delete;
+
+		//This retrieves a pointer to the current instance of ResourceManager. If it doesn't exist, then one will be created and returned.
+		static ResourceManager* getInstance();
+
+		#pragma endregion
+
+		#pragma region Set Functions
+
+		//Function to set icon for the application.
+		void setAppIcon(GLFWwindow&);
+
+		//Function to save all file paths into m_filePaths within the provided path, including subdirectories.
+		void saveFilePaths(const std::string& path);
+		#pragma endregion
+
+		#pragma region Get Functions
+
+		//Function to return a json (formatted as jsons from nlohmanns library) from the hashmap based on a provided name. Returns a nullptr if no json is found.
+		nlohmann::json getJsonData(const std::string& name);
+
+		GLuint getTexture(const std::string& name);
+
+
+		//Function to return a shader (formatted as a string with newlines to seperate GSLS code) from the hashmap based on a provided name. Returns an empty string if no shader is found.
+		std::string getShaderData(const std::string& name);
+		#pragma endregion
+
+
+	protected:
+		#pragma region Constructors & Destructors
+
+		//Constructor for ResourceManager Class. Finds all files under the 'data' path and stores the paths in a map. 
+		ResourceManager();
+
+		//Destructor
+		~ResourceManager();
+		#pragma endregion
+
+	private:
+
+		//The static instance of ResourceManager that will be returned if the resource manager is requested.
+		static ResourceManager* m_pinstance;
+
+		//static lock m_mutex is the lock to be used for static functions. Otherwise, m_functionlock should be utilized.
+		static std::mutex m_mutex;
+		std::mutex m_functionLock;
+
+		int m_RGB { 3 }, m_RGBA { 4 };
+			
+		#pragma region File Extension Variables
+			
+		//Extensions for all files handled through resource manager.
+		std::string m_jsonFileExt { "json" };
 		
-		public:
-
-			#pragma region Singleton Instance Management
-
-			//Singletons should not be cloneable, this is to prevent clones.
-			ResourceManager(ResourceManager &other) = delete;
+		std::vector<std::string> m_shaderFileExts { "vs", "fs" };
 			
-			//Singletons should not be assignable, this is to prevent that.
-			void operator=(const ResourceManager &) = delete;
-			
-			//This retrieves a pointer to the current instance of ResourceManager. If it doesn't exist, then one will be created and returned.
-			static ResourceManager* getInstance();
-			
-			#pragma endregion
-
-			#pragma region Set Functions
-
-			//Function to save all file paths into m_filePaths within the provided path, including subdirectories.
-			void saveFilePaths(const std::string& path);
-			#pragma endregion
-
-			#pragma region Get Functions
-			
-			//Function to return a json from the hashmap based on a provided name. Returns a nullptr if no json is found.
-			nlohmann::json getJsonData(const std::string& Name);
-
-			//Function to return a shader from the hashmap based on a provided name. Returns an empty string if no shader is found.
-			std::string getShaderData(const std::string& Name);
-			#pragma endregion
-
+		std::vector<std::string> m_imageFileExts { "jpeg", "png", "jpg"};
+		#pragma endregion
 		
-		protected:
-			#pragma region Constructors
-			
-						//Constructor for ResourceManager Class. Finds all files under the 'data' path and stores the paths in a map. 
-						ResourceManager();
-			#pragma endregion
+		#pragma region File Storage Variables
+						
+		//Icon related functions.
+		std::string m_iconPath { std::filesystem::current_path().parent_path().string() + "\\Engine\\BlueDemiseIcon.png" };
+						
+		//Every file path found under the specified resources folder, 'm_sourcePath'.
+		std::unordered_map<std::string, std::string> m_filePaths{};
 		
-		private:
-
+		//Map to store each processed texture containing an image imported from STBI
+		std::unordered_map<std::string, GLuint> m_textures{};
 			
-
-			//The static instance of ResourceManager that will be returned if the resource manager is requested.
-			static ResourceManager* m_pinstance;
+		//A map to store Json files, utilizing the json library.
+		std::unordered_map<std::string, nlohmann::json> m_jsons{};
 			
-			//static lock m_mutex is the lock to be used for static functions. Otherwise, m_functionlock should be utilized.
-			static std::mutex m_mutex;
-			std::mutex m_functionLock;
+		//A map to store shader files as strings
+		std::unordered_map<std::string, std::string> m_shaders{};
+
+		//Vector containing all source paths to search for files (think of these as the 'root' asset folders). 
+		//This can be added to from Application/Game code for custom paths.
+		std::vector<std::string> m_sourcePaths { std::filesystem::current_path().string() + "\\Data" };
+		#pragma endregion
 			
-			#pragma region File Storage Variables
-			
-			//Extensions for all files handled through resource manager.
-			std::string m_jsonFileExt = "json";
-
-			std::vector<std::string> m_shaderFileExts = { "vs", "fs" };
-			#pragma endregion
-
-			#pragma region File Storage Variables
-			
-						//Every file path found under the specified resources folder, 'm_sourcePath'.
-						std::unordered_map<std::string, std::string> m_filePaths{};
-
-						//For image files. Not sure what's imported yet. @Cole, you can modify this. STB is the library currently being used.
-						//std::map<std::string, int> m_images{ {"Enemy", 10}, {"Player", 15}, {"Tile", 20}, };
-
-						//A map to store Json files, utilizing the json library.
-						std::unordered_map<std::string, nlohmann::json> m_jsons{};
-			
-						//A map to store shader files as strings
-						std::unordered_map<std::string, std::string> m_shaders{};
-
-						//Vector containing all source paths to search for files (think of these as the 'root' asset folders). 
-						//This can be added to from Application/Game code for custom paths.
-						std::vector<std::string> m_sourcePaths = { std::filesystem::current_path().string() + "\\Data" };
-
-			#pragma endregion
-			
-			#pragma region Set & Read/Load Functions
+		#pragma region Set & Read/Load Functions
 		
-						//Add file path from provided directory_entry to the m_filePaths map.
-						void saveFilePath(std::filesystem::directory_entry path);
-
-						//Read file found at sourcePath and return it as a string. Will only read the provided string. Returns an empty string if nothing is found.
-						std::string readSource(const std::string &sourcePath);
-			#pragma endregion
+		//Add file path from provided directory_entry to the m_filePaths map.
+		void saveFilePath(std::filesystem::directory_entry path);
+		
+		//Function to return an image (formatted as a pointer to an unsigned char) from the hashmap based on a provided name. Returns a nullptr if no image is found.
+		//NOTE: After using the image, you MUST use stbi_image_free(); in order to free the memory of the image.
+		ImageData readImageData(const std::string& name);
+		
+		//Read file found at sourcePath and return it as a string. Will only read the provided string. Returns an empty string if nothing is found.
+		std::string readSource(const std::string &sourcePath);
+		#pragma endregion
 	};
 }
