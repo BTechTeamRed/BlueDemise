19336097f141d89b560e0c6cca86a0f7f45955a6
James Pike <46502411+pike983@users.noreply.github.com>
2022-10-21 01:05:27 -0700

    Added alternative search paths for audio files.
    Added alternative search paths for audio files and changed the files to play from the Game and Engine media folders. Added other file types to the CMake so audio files can be included in builds.

--- Engine/src/Engine/Audio.cpp
+++ Engine/src/Engine/Audio.cpp
@@ -1,4 +1,5 @@
 #include "Audio.h"
+#include <string>
 
 using namespace std;
 using namespace irrklang;
@@ -19,38 +20,62 @@ AudioPlayerSingleton* AudioPlayerSingleton::getInstance()
 // Plays a sound, takes a path to the sound as a const char, a bool if the sound should loop,
 // a bool if the sound should be initialized paused or play on initialization, and a bool for if
 // sound effects should be used. Sound effects need to be added after using this function using
-// the returned ISound*.
+// the returned ISound*.  If the sound cannot be found, it returns a nullptr.
 // More information on sound effects: https://www.ambiera.com/irrklang/docu/index.html#effects
 // 
-ISound* AudioPlayerSingleton::playSound(const char* soundPath, bool loop, bool startPaused, bool useSoundEffects)
+ISound* AudioPlayerSingleton::playSound(const char* soundName, bool loop, bool startPaused, bool useSoundEffects)
 {
-	ISound* theSound = engine->play2D(soundPath, loop, startPaused, true, ESM_AUTO_DETECT, useSoundEffects);
+	string filePath = "../../GameEngine/Engine/src/Media/";
+	filePath += soundName;
+	const char* fullPath = filePath.c_str();
+	ISound* theSound = engine->play2D(fullPath, loop, startPaused, true, ESM_AUTO_DETECT, useSoundEffects);
 	if (theSound != 0)
 	{
 		return theSound;
 	}
-	return nullptr;
-	//TODO: implement alternate media folder search if multiple media folders
-	// (for game and engine for example) are implemented
+	else
+	{
+		string filePath2 = "../../GameEngine/Game/src/Media/";
+		filePath2 += soundName;
+		const char* fullPath2 = filePath2.c_str();
+		theSound = engine->play2D(fullPath, loop, startPaused, true, ESM_AUTO_DETECT, useSoundEffects);
+		if (theSound != 0)
+		{
+			return theSound;
+		}
+		return nullptr;
+	}
 }
 
 // Plays a sound in 3D space, takes a const char* path to the sound, 
 // an irrklang::vec3df position (irrklang::vec3df positionName(x,y,z)),
 // a bool if the sound should loop, a bool if the sound should initialize paused,
 // and a bool if the sound should use sound effects. Sound effects need to be added after
-// using this funtion using the returned ISound*.
+// using this funtion using the returned ISound*. If the sound cannot be found, it returns a nullptr.
 // More information on sound effects: https://www.ambiera.com/irrklang/docu/index.html#effects
 // 
-ISound* AudioPlayerSingleton::play3DSound(const char* soundPath, vec3df sound3DPosition, bool loop, bool beginPaused, bool useSoundEffects)
+ISound* AudioPlayerSingleton::play3DSound(const char* soundName, vec3df sound3DPosition, bool loop, bool beginPaused, bool useSoundEffects)
 {
-	ISound* theSound = engine->play3D(soundPath, sound3DPosition, loop, beginPaused, true, irrklang::ESM_AUTO_DETECT, useSoundEffects);
+	string filePath = "../../GameEngine/Engine/src/Media/";
+	filePath += soundName;
+	const char* fullPath = filePath.c_str();
+	ISound* theSound = engine->play3D(fullPath, sound3DPosition, loop, beginPaused, true, irrklang::ESM_AUTO_DETECT, useSoundEffects);
 	if (theSound != 0)
 	{
 		return theSound;
 	}
-	return nullptr;
-	//TODO: implement alternate media folder search if multiple media folders
-	// (for game and engine for example) are implemented
+	else
+	{
+		string filePath2 = "../../GameEngine/Game/src/Media/";
+		filePath2 += soundName;
+		const char* fullPath2 = filePath2.c_str();
+		ISound* theSound = engine->play3D(fullPath2, sound3DPosition, loop, beginPaused, true, irrklang::ESM_AUTO_DETECT, useSoundEffects);
+		if (theSound != 0)
+		{
+			return theSound;
+		}
+		return nullptr;
+	}
 }
 
 // Use on an ISound* to clear memory space when the sound is no longer needed.
