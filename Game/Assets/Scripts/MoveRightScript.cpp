2a1096dbcbc5f62f93a5895ffd86104d7f4d9772
Martin <mvanlaethem0@gmail.com>
2022-11-18 20:55:11 -0800

    Set up scene

--- Game/Assets/Scripts/MoveRightScript.cpp
+++ Game/Assets/Scripts/MoveRightScript.cpp
@@ -5,6 +5,6 @@ namespace Engine
 	void MoveRightScript::onUpdate(const Engine::DeltaTime& dt)
 	{
 		auto& c = getComponent<Engine::TransformComponent>();
-		c.position.x += 5 * dt;
+		//c.position.x += 5 * dt;
 	}
 }
